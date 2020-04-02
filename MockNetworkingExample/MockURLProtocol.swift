2e7aa2eba734a1003217ca22e470e34fc2eb6f7d
“ashokvadivelu” <ashokatonline@gmail.com>
2020-04-02 15:11:11 +0530

    initial commit

---
+++ MockNetworkingExample/MockURLProtocol.swift
@@ -0,0 +1,40 @@
+//
+//  MockNetworkingProtocol.swift
+//  MockNetworkingExample
+//
+//  Created by fordlabs on 02/04/20.
+//  Copyright © 2020 ashok. All rights reserved.
+//
+
+import Foundation
+class MockURLProtocol: URLProtocol {
+    // this dictionary maps URLs to test data
+    static var stub = [URL?: Data]()
+
+    // say we want to handle all types of request
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    // ignore this method; just send back what we were given
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        // if we have a valid URL…
+        if let url = request.url {
+            // …and if we have test data for that URL…
+            if let data = MockURLProtocol.stub[url] {
+                // …load it immediately.
+                self.client?.urlProtocol(self, didLoad: data)
+            }
+        }
+
+        // mark that we've finished
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+
+    // this method is required but doesn't need to do anything
+    override func stopLoading() { }
+}
---
+++ MockNetworkingExample/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  MockNetworkingExample
+//
+//  Created by fordlabs on 02/04/20.
+//  Copyright © 2020 ashok. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+public struct userEncode: Codable {
+    var first_name: String
+    var last_name: String
+    var age: Int
+}
+
+//Note: Enable the line 48 and 49 to check mock response data
+class ViewController: UIViewController {
+
+    // url
+    let url = URL(string: "https://learnappmaking.com/ex/users.json")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+         neworkingApiRequest()
+
+    }
+
+    var mockEncoderValue: Data {
+        let encode = userEncode(first_name: "ashok", last_name: "kumar", age: 27)
+        let arrayEncode = [encode]
+        let encoderValue =  try! JSONEncoder().encode(arrayEncode)
+        return encoderValue
+    }
+
+ // Networking Setup
+    private func neworkingApiRequest() {
+        // Networking Api
+
+        // now set up a configuration
+        let config = URLSessionConfiguration.ephemeral
+
+
+        // Enable the below two line to check mock data response
+        //MockURLProtocol.stub = [url: mockEncoderValue]
+       // config.protocolClasses = [MockURLProtocol.self]
+
+        // and create the URLSession from that
+
+        let session = URLSession(configuration: config)
+        let urlString = URL(string: "https://learnappmaking.com/ex/users.json")!
+        let task = session.dataTask(with: urlString) { data, response, error in
+            do {
+                if let userData = data {
+                print(userData)
+                let decodedValue =  try JSONDecoder().decode([userEncode].self, from: userData)
+                    print(decodedValue[0].first_name)
+                }
+            }
+            catch {
+                    print("catch")
+            }
+            }
+        task.resume()
+    }
+
+}
+
+
+//Mock protocol class
+
